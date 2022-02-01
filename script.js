d1db8f80d3270b10fa1a2b5a6bc179469eefc0f8
Viktor Kruk <viktkruk@gmail.com>
2022-02-01 17:38:51 +0200

    desktop

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// Swiper
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 10,
+  speed: 1000,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
